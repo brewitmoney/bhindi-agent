b56e557a350b92316748037548ffc27770a78e5a
Koshik Raj <koshik.raj@gmail.com>
2025-09-12 15:50:58 +0530

    chore: updates eslint

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,6 @@ export default tseslint.config(
     },
   },
   {
-    ignores: ['dist/', 'node_modules/', '*.config.js'],
+    ignores: ['dist/', 'node_modules/', '*.config.js', '*.config.cjs', 'ecosystem.config.cjs'],
   }
 ); 
